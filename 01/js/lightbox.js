5f371aef689dc771b0c37bdd08e935b8067be1f1
JoanAlvarado040700 <sevaslarios@gmail.com>
2022-11-29 18:16:56 -0600

    ligthbox

--- 01/js/lightbox.js
+++ 01/js/lightbox.js
@@ -1,3 +1,24 @@
 const imagenes = document.querySelectorAll('.img-galery')
 const imagenesList = document.querySelector('.agregar-imagen')
-const contenedorList = document.querySelector('.imagen-light')
+const contenedorLight = document.querySelector('.imagen-light')
+const hamburguesa1 = document.querySelector('.hamburguesa')
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () =>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorLight.addEventListener('click',(e) =>{
+    if(e.target != imagenesList){
+        contenedorLight.classList.toggle('show')
+        imagenesList.classList.toggle('showImage')
+        hamburguesa1.style.opacity ='1'
+    }
+})
+
+const aparecerImagen = (imagen) => {
+    imagenesList.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesList.classList.toggle('showImage')
+    hamburguesa1.style.opacity ='0'
+}
